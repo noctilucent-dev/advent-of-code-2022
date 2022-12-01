b5e9d7243b7920728a5113c4f3f831711d77817b
Nathan Richards <nls@logios.co.uk>
2022-12-01 11:52:06 +0000

    Cleaned up day 1

--- day01/index.js
+++ day01/index.js
@@ -1,39 +1,33 @@
-let { raw } = require("../util");
+let { DEBUG, raw } = require("../util");
 
-// raw = `1000
-// 2000
-// 3000
+if (DEBUG) {
+    raw = `1000
+2000
+3000
 
-// 4000
+4000
 
-// 5000
-// 6000
+5000
+6000
 
-// 7000
-// 8000
-// 9000
+7000
+8000
+9000
 
-// 10000`;
+10000`;
+}
 
 function part1(lines) {
-    const elves = [];
     let sum = 0;
     let max = 0;
     for (let i=0; i<lines.length; i++) {
-        console.log(lines[i]);
         if (lines[i].length === 0) {
-            elves.push(sum);
-            if (sum > max) max = sum;
-            console.log(`Elf: ${sum}, Max: ${max}`);
             sum = 0;
         } else {
             sum += Number(lines[i]);
+            max = Math.max(sum, max);
         }
     }
-
-    elves.push(sum);
-    if (sum > max) max = sum;
-    console.log(`Elf: ${sum}, Max: ${max}`);
 
     return max;
 }
@@ -42,10 +36,8 @@ function part2(lines) {
     const elves = [];
     let sum = 0;
     for (let i=0; i<lines.length; i++) {
-        console.log(lines[i]);
         if (lines[i].length === 0) {
             elves.push(sum);
-            console.log(`Elf: ${sum}`);
             sum = 0;
         } else {
             sum += Number(lines[i]);
@@ -53,17 +45,15 @@ function part2(lines) {
     }
 
     elves.push(sum);
-    console.log(`Elf: ${sum}`);
-
+    
     elves.sort((a, b) => a - b);
-    console.log(elves);
-    console.log(elves.slice(-3));
-    console.log(elves.slice(-3).reduce((p, c) => p+c));
-
     return elves.slice(-3).reduce((p, c) => p+c);
 }
 
-const max = part1(raw.trim().split("\n"));
-console.log(max);
+const lines = raw.trim().split("\n");
 
-console.log(part2(raw.trim().split("\n")));
+const max = part1(lines);
+console.log(`Part1: ${max}`);
+
+const top3 = part2(lines);
+console.log(`Part2: ${top3}`);
