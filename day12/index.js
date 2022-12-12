e4fa8331b438e76430402385c10341c9e2d9d80f
Nathan Richards <nls@logios.co.uk>
2022-12-12 14:31:07 +0000

    Solved day 12 part 2

--- day12/index.js
+++ day12/index.js
@@ -114,6 +114,70 @@ function part1(start, end, grid) {
     }
 }
 
+function part2(start, end, grid) {
+    const startNode = new Node(start[0], start[1], 0, undefined);
+    const graph = new Graph(startNode);
+
+    const aPositions = [];
+
+    for(let y=0; y<grid.length; y++) {
+        for(let x=0; x<grid[y].length; x++ ){
+            if (grid[y][x] === 0) {
+                aPositions.push([x, y]);
+                const leaf = new Node(x, y, 0, undefined);
+                graph.nodes.push(leaf);
+                graph.leaves.push(leaf);
+            }
+        }
+    }
+
+    while(graph.leaves.length > 0) {
+        const leaf = graph.getBestLeaf(n => n.cost + n.distanceTo(end[0], end[1]));
+        const { x, y } = leaf;
+
+        const height = grid[y][x];
+
+        let nextSteps = [];
+
+        if (y > 0 && grid[y-1][x] - height <= 1) {
+            nextSteps.push([x, y-1]);
+        }
+        if (y < grid.length-1 && grid[y+1][x] - height <= 1) {
+            nextSteps.push([x, y+1]);
+        }
+        if (x < grid[0].length - 1 && grid[y][x+1] - height <= 1) {
+            nextSteps.push([x+1, y]);
+        }
+        if (x > 0 && grid[y][x-1] - height <= 1) {
+            nextSteps.push([x-1, y]);
+        }
+
+        nextSteps = nextSteps.sort((a, b) => distance(a, end) - distance(b, end));
+
+        for(let i=0; i<nextSteps.length; i++) {
+            const [nx, ny] = nextSteps[i];
+            const matching = graph.nodes.find(n => n.x === nx && n.y === ny);
+            if (matching) {
+                if (matching.cost > leaf.cost + 1) {
+                    matching.cost = leaf.cost + 1;
+                    matching.parent = leaf;
+                }
+            } else {
+                const newLeaf = new Node(nx, ny, leaf.cost + 1, leaf);
+                graph.nodes.push(newLeaf);
+                graph.leaves.push(newLeaf);
+            }
+        }
+    }
+
+    const endNode = graph.nodes.find(n => n.x === end[0] && n.y === end[1])
+    let c = endNode;
+    while(grid[c.y][c.x] > 0) {
+        c = c.parent;
+    }
+    return endNode.cost - c.cost;
+}
+
 const { start, end, grid } = parse(raw.split("\n"));
 
 log(print(grid, 2, " "));
@@ -121,3 +185,6 @@ log("\n");
 
 const p1 = part1(start, end, grid);
 console.log(`Part1: ${p1}`);
+
+const p2 = part2(start, end, grid);
+console.log(`Part2: ${p2}`);
